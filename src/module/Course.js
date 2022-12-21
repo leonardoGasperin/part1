79f02a1bf33d6a0c2ab6ad3947d1ad8ba0221f7d
LeonardoGasperin <gasperinlvg@gmail.com>
2022-12-21 15:14:51 -0300

    clear part1B

---
+++ src/module/Course.js
@@ -0,0 +1,49 @@
+const course = {
+    name : 'Half Stack application development',
+    parts: [
+        {
+        part: 'Fundamentals of React',
+        exercicies: 10
+        },
+        {
+        part: 'Using props to pass data',
+        exercicies: 7
+        },
+        {
+        part: 'State of a component',
+        exercicies: 14
+        }
+    ]
+}
+
+function TotalExercicies () {
+    let total = 0
+
+    course.parts.forEach(part => {
+    total += part.exercicies    
+    });
+
+    return total
+}
+
+const total = {
+    mensege: 'Number of exercises:'
+}
+
+export const Content = () => {
+    return(
+        <div>
+            <h1>{course.name}</h1>
+            {course.parts != null && (
+                <div>
+                {course.parts.map((part => (
+                    <p>{part.part}. Total exercicies: {part.exercicies}</p>
+                )))}
+                <p>{total.mensege} {TotalExercicies()}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Content;
