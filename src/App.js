72bc7209a7b5e9428221718e52e0da813d04d20d
LeonardoGasperin <gasperinlvg@gmail.com>
2022-12-21 15:01:21 -0300

    part1B

--- src/App.js
+++ src/App.js
@@ -1,40 +1,64 @@
 function App() {
-  const course = 'Half Stack application development'
+  /*const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercicies1 = 10
   const part2 = 'Using props to pass data'
   const exercicies2 = 7
   const part3 = 'State of a component'
   const exercicies3 = 14
+*/
 
-  const Header = (props) => {
-    return <h1>{props.course}</h1>
+
+  const course = {
+    name : 'Half Stack application development',
+    parts: [
+      {
+        part: 'Fundamentals of React',
+        exercicies: 10
+      },
+      {
+        part: 'Using props to pass data',
+        exercicies: 7
+      },
+      {
+        part: 'State of a component',
+        exercicies: 14
+      }
+    ]
   }
 
-  const Part = (part, exercicies) => {
-    return <p>{part.name} {exercicies.points}</p>
-  }
+ function TotalExercicies () {
+  let total = 0
+  
+  course.parts.forEach(part => {
+    total += part.exercicies    
+  });
+
+  return total
+}
+  const total = {
+    mensege: 'Number of exercises:'
+   }
+
 
   const Content = () => {
     return(
       <div>
-        <Part name={part1} points={exercicies1}/>
-        <Part name={part2} points={exercicies2}/>
-        <Part name={part3} points={exercicies3}/>
+        <h1>{course.name}</h1>
+        {course.parts != null && (
+          <div>
+            {course.parts.map((part => (
+              <p>{part.part}. Total exercicies: {part.exercicies}</p>
+            )))}
+            <p>{total.mensege} {TotalExercicies()}</p>
+          </div>
+        )}
       </div>
     )
   }
 
-  function Total(){
-    return <p>Number of exercises {exercicies1+exercicies2+exercicies3}</p>
-  }
-
   return (
-    <div>
-      <Header course={course}/>  
-      <Content name={part1} points={exercicies1}/>
-      <Total/>
-    </div>
+    <Content/>
   );
 }
 
